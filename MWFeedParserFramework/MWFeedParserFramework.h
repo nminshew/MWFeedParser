a4e53cb6e6f441f62b2b8d1f0dd68e8f3b75c751
Nate Minshew <nminshew@gmail.com>
2012-12-01 10:55:16 -0600

    Added framework target.

---
+++ MWFeedParserFramework/MWFeedParserFramework.h
@@ -0,0 +1,13 @@
+//
+//  MWFeedParserFramework.h
+//  MWFeedParserFramework
+//
+//  Created by Nate Minshew on 12/1/12.
+//  Copyright (c) 2012 Michael Waterfall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWFeedParserFramework : NSObject
+
+@end
---
+++ MWFeedParserFramework/MWFeedParserFramework.m
@@ -0,0 +1,13 @@
+//
+//  MWFeedParserFramework.m
+//  MWFeedParserFramework
+//
+//  Created by Nate Minshew on 12/1/12.
+//  Copyright (c) 2012 Michael Waterfall. All rights reserved.
+//
+
+#import "MWFeedParserFramework.h"
+
+@implementation MWFeedParserFramework
+
+@end
